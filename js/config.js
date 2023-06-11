24e4e0cc0c1866dfccbd96957eab1ecd3a94a647
AmanSarraf <amansarrafnit@gmail.com>
2023-06-11 12:53:43 +0530

    added API invokeUrl

--- js/config.js
+++ js/config.js
@@ -5,6 +5,6 @@ window._config = {
         region: 'ap-south-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://9wkjtndi3h.execute-api.ap-south-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
